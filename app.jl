3fe526f03411ebe6383f932a5a65b599f345263f
volarex84 <guillaume.marie84@gmail.com>
2023-02-15 14:30:22 +0100

    None

--- app.jl
+++ app.jl
@@ -1,13 +1,11 @@
 
-push!(LOAD_PATH, "ThinningTrajectories/")
-using ThinningTrajectories
 using Dash
 using PlotlyJS
 using DataFrames
 
-
+include("ThinningTrajectories/src/ThinningTrajectories.jl")
 include("Experiment_setup.jl")
-ORCres_folder="/home/guigeek/Julia_script/orc/YE/"
+
 
 n_poly = [2,3,4,5]
 rdistart = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
