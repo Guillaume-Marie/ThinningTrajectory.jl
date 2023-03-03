0e2ea4e2a793b7150d28b2c66fd0bef4c1af863d
volarex84 <guillaume.marie84@gmail.com>
2023-03-03 13:49:57 +0100

    None

--- test.jl
+++ test.jl
@@ -4,17 +4,25 @@ using ThinningTrajectories
 using DataFrames
 using StatsPlots
 include("Experiment_setup.jl")
-version = "v0.7"
+version = "v0.11"
 ORCres_folder = "/home/guigeek/Julia_script/orc/Output_$(version)/YE/"
 ORCres_4dim = "/home/guigeek/Julia_script/orc/Output_$(version)/MO/"
 
 # evergreen temperate conifer : PFT 2,3,4
-spruce = ThinningTrajectories.estimate_θrdi(3, Sexp)
-ThinningTrajectories.merge_previous_plots(spruce, ORCres_folder, version, Sexp)
+spruce = ThinningTrajectories.estimate_θrdi(2, Sexp)
+pp_s = [
+    [0.45842631077055873, -0.008054207847051202, 0.0015052315685268529, -3.177845420230941e-5],
+    [0.08983397818342638, 0.02849620880030135, -0.0002007047693042513, -8.974292980417947e-6]
+]
+ThinningTrajectories.merge_previous_plots(spruce, ORCres_folder, version, Sexp, poly_param=pp_s)
 
 # deciduous temperate broadleaved : PFT 5,6,7
-oak = ThinningTrajectories.estimate_θrdi(6, Sexp)
-ThinningTrajectories.merge_previous_plots(oak, ORCres_folder, version, Sexp)
+oak = ThinningTrajectories.estimate_θrdi(5, Sexp)
+pp_o = [
+    [0.7373714729598279,-0.015416856258914405,0.0009959200016314317,-1.536439822186209e-5],
+    [0.4721525464343815,0.0003699592758440316,0.0003666212967303307,-8.120450529662615e-6]
+]
+ThinningTrajectories.merge_previous_plots(oak, ORCres_folder, version, Sexp, poly_param=pp_o)
 
 orcr = ThinningTrajectories.merge_netcdf_4dim(
     ORCres_4dim, ["CCIND", "CCDIAMETER"], "stomate", Sexp)
@@ -30,12 +38,14 @@ orcr_dia = DataFrame(
     :param => orcr_diadom[:, "param"],
     :time => orcr_diadom[:, "time"])
 
-orcr_dia = filter(:pft => (==("deciduous temperate broadleaved")), orcr_dia)
-#orcr_dia = filter(:ver => (==("Low Taumax")), orcr_dia)
+orcr_dia_o = filter(:pft => (==("deciduous temperate broadleaved")), orcr_dia)
+orcr_dia_s = filter(:pft => (==("evergreen temperate conifer")), orcr_dia)
 
-orcr_diadom = filter(:pft => (==("deciduous temperate broadleaved")), orcr_diadom)
+orcr_diadom_o = filter(:pft => (==("deciduous temperate broadleaved")), orcr_diadom)
+orcr_diadom_s = filter(:pft => (==("evergreen temperate conifer")), orcr_diadom)
 
-@df orcr_diadom plot(
+
+@df orcr_diadom_s plot(
     :time,
     :value,
     group=(:ver, :inc),
@@ -43,7 +53,7 @@ orcr_diadom = filter(:pft => (==("deciduous temperate broadleaved")), orcr_diado
     legend = false,
 )
 
-@df orcr_dia plot(
+@df orcr_dia_s plot(
     :time,
     :value*1000,
     group=(:ver, :inc),
@@ -65,6 +75,10 @@ using Polynomials
 @. dia_lin(x, θ) = θ[1] * x 
 @. dia_poly(x, θ) = θ[1] + θ[2]* x + θ[3]* x^2 +  θ[4]* x^3
 
+
+dia_poly(8.229, pp[1])
+dia_poly(8.229, pp[2])
+
 WEIBULL_PARAM = [
     [3.0,7.5,10.0,20.0,30.0,40.0,45.0],
     [1.2,1.2,1.5,4.0,8.0,16.0,25.0]] # λ, k
@@ -81,6 +95,7 @@ plot(WEIBULL_PARAM[1],WEIBULL_PARAM[2],
     label="Weibull parameters",
     ylab="k", xlab="λ")
 
+cc= -0.08
 plot(0.01:0.5:50, WEIBULL_CDF(0.01:0.5:50, 3.0, dia_exp(3.0, cc)))
 plot!(0.01:0.5:50, WEIBULL_CDF(0.01:0.5:50, 7.5, dia_exp(7.5, cc)))
 plot!(0.01:0.5:50, WEIBULL_CDF(0.01:0.5:50, 10.0, dia_exp(10.0, cc)))
@@ -116,11 +131,11 @@ end
 
 Wiebull_circ_class_prop(3.0, 1.2, 0.01, 3, 50)
 
-plot(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 3.0, 1.1))
-plot!(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 7.5, 1.4))
-plot!(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 10.0, 1.3))
+plot(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 3.0, 1.2))
+plot!(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 7.5, 1.2))
+plot!(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 10.0, 1.2))
 plot!(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 15.0, 1.2))
-plot!(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 20.0, 1.0))
+plot!(0.01:0.5:50, WEIBULL_PDF(0.01:0.5:50, 20.0, 1.2))
 
 
 
